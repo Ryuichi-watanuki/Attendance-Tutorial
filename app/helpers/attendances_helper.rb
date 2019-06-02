ce869d3101e8fd9c48daa17642a1bd7bbc8958fd
Watanuki <watanuki@email.com>
2019-06-02 06:33:50 +0000

    勤怠表示時間の変更対応

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,32 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+  # 出勤時間を15分単位にして返します（切り上げ）
+  def started_at_to_15_in_minutes(started_at)
+    case started_at.min
+    when 0..14
+      started_at.change(min: 15)
+    when 15..29
+      started_at.change(min: 30)
+    when 30..44
+      started_at.change(min: 45)
+    when 45..59
+      started_at.change(hour: started_at.hour + 1, min: 0)
+    end
+  end
+  
+  # 退勤時間を15分単位にして返します（切り捨て）
+  def finished_at_to_15_in_minutes(finished_at)
+    case finished_at.min
+    when 0..14
+      finished_at.change(min: 0)
+    when 15..29
+      finished_at.change(min: 15)
+    when 30..44
+      finished_at.change(min: 30)
+    when 45..59
+      finished_at.change(min: 45)
+    end
+  end
 end
